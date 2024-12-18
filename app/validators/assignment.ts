dfc6319fbf017cb0b709c2ac5b49401c6249f66b
Иван Шепелев <9mix@Ivans-MacBook-Pro.local>
2024-12-18 12:13:43 +0300

    Update Migration and Seeders - Assignments. Add Validators for Assignments and Create SimpleMessagesProvider

--- app/validators/assignment.ts
+++ app/validators/assignment.ts
@@ -13,6 +13,6 @@ export const updateAssignmentsValidator = vine.compile(
 vine.object({
     module_id: vine.number(),
     title: vine.string().trim(),
-    description: vine.string().trim(),
+    description: vine.string().trim().optional(),
 })
 )
